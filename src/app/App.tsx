a7120b8d4db84384c54ce8a429646c839493c2ca
Andrew Brunker <andrew.brunker@lymlive.com.au>
2023-09-08 21:10:39 +1000

    fetch hookin for the json data
    Signed-off-by: Andrew Brunker <andrew.brunker@lymlive.com.au>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/app/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const endPoint = 'http://localhost:3000'
+
+    useEffect(() => {
+        fetch(`${endPoint}/load/dataJson`, {
+            mode: 'no-cors'
+        })
+        .then(data => console.log('data', data))
+        .catch(err => console.log('ERROR', err))
+    }, [])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/app/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
