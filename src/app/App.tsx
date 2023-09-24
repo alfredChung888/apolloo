a3ebd8fca6b3fb0ddbd39a6f27b88fc85dca5297
Elliot <elliotmason64@gmail.com>
2023-09-24 10:44:55 +1000

    removed defunct code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -50,49 +50,6 @@ function App() {
     return hoursStr + ":" + minutesStr;
   }
 
-  //create a singular card element
-  function Cards({ index }: { index: number }) {
-    const data = responseData.data.data.data[index];
-    const idKey = data.hasOwnProperty('adverts_id') ? 'adverts_id' : 'advert_playlist_id';
-    const nameKey = data.hasOwnProperty('adverts_name') ? 'adverts_name' : 'playlist_name';
-    const useridKey = data.hasOwnProperty('adverts_user_id') ? 'adverts_user_id' : 'client_id';
-    const userIdLabel = useridKey === 'client_id' ? 'Client ID' : 'User ID';
-    var time = data.adverts_refresh_time;
-    time = minutesToHHMM(time);
-
-    //define the label type
-    let labelText = '';
-    if (idKey === 'adverts_id') {
-      labelText = 'Advert';
-    } else if (idKey === 'advert_playlist_id') {
-      labelText = 'Playlist';
-    }
-
-    return (
-      <div className="card">
-        {responseData.data ? (
-          <>
-            <div className="title">
-              {JSON.stringify(data[idKey], null, 2)}: {JSON.parse(JSON.stringify(data[nameKey], null, 2))}
-            </div>
-            <div className="label">
-              {labelText}
-            </div>
-            <div className="content_card">
-              <span className="bold_text">{userIdLabel}:</span><span className="text"> {JSON.stringify(data[useridKey], null, 2)}</span>
-            </div>
-            {userIdLabel !== "Client ID" && (
-              <div className="content_card">
-                <span className="bold_text">Refresh Time: </span><span className="text">{JSON.parse(JSON.stringify(time, null, 2))} mins </span>
-              </div>
-            )}
-          </>
-        ) : (
-          <p>No data available</p>
-        )}
-      </div>
-    );
-  }
   function formatDate(dateString: string | null | undefined): string {
     if (!dateString) {
       return "N/A"; // Handle cases where the date string is not available
@@ -106,13 +63,6 @@ function App() {
 
     const date = new Date(dateString);
     return date.toLocaleDateString(undefined, options);
-  }
-
-  const cardIndices = responseData.data ? Array.from(Array(responseData.data.data.data.length).keys()) : [];
-
-  const cardComponents = [];
-  for (let i = 0; i < cardIndices.length; i++) {
-    cardComponents.push(<Cards key={i} index={cardIndices[i]} />);
   }
 
   return (
@@ -158,10 +108,6 @@ function App() {
                 </div>
               </div>
               <div className="content-wrapper">
-
-
-              </div>
-              <div className="content-wrapper">
                 <div className="info_card">
                   <span className="bold_text">
                     Last Sync:
@@ -174,7 +120,6 @@ function App() {
                         "Loading..." // You can display a loading message here while data is being fetched
                       )
                     }
-
                   </span>
                 </div>
                 <div className="info_card">
